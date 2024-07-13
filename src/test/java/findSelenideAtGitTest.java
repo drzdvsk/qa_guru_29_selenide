04d4d13d57178a734f0be16714462f8e2e6b4219
Andrei Drazdouski <a.drzdvsk@gmail.com>
2024-07-13 19:04:40 +0300

    changes after verification

---
+++ src/test/java/findSelenideAtGitTest.java
@@ -0,0 +1,34 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.files.DownloadActions.click;
+
+public class findSelenideAtGitTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    public void findSelenideTest() {
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $$("a[data-view-component='true'].Truncate-text")
+                .findBy(href("/selenide/selenide/wiki/SoftAssertions"))
+                .shouldBe(visible)
+                .click();
+        $("a#user-content-3-using-junit5-extend-test-class")
+            .shouldHave(href("#3-using-junit5-extend-test-class"))
+            .scrollIntoView(true);
+        System.out.println("example for JUnit 5 is found");
+    }
+}
