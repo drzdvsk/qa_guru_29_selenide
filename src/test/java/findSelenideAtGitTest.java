b1a366b39584c37ffcf3618222de7aeb03655650
Andrei Drazdouski <117362052+drzdvsk@users.noreply.github.com>
2024-07-14 00:07:04 +0300

    Update name of class

--- src/test/java/findSelenideAtGitTest.java
+++ src/test/java/findSelenideAtGitTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
-public class findSelenideAtGitTest {
+public class FindSelenideAtGitTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
